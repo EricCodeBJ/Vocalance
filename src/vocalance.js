5e5d32cf886011744afbbdf62ced9ce78a47b93f
DEKOUN Cédric <83883583+EricCodeBJ@users.noreply.github.com>
2022-10-19 00:16:25 +0200

    finish v1

--- src/vocalance.js
+++ src/vocalance.js
@@ -20,7 +20,7 @@ class VocalanceJS {
         <h5 class="title">
           <img
             alt="icon-boot"
-            src="https://raw.githubusercontent.com/EricCodeBJ/Vocalance/ee7a35127a6ab9ffe8275389d0fd9d815c6c0b82/icon/chatbox.png"
+            src="https://www.kidevs.xyz/vocalance/lib/assets/icon/chatbox.png"
           />
           Vocalance
         </h5>
@@ -30,7 +30,7 @@ class VocalanceJS {
           <img
             class="vw-chat-badge-success vocalance-widget-hide"
             alt="icon-statut"
-            src="https://raw.githubusercontent.com/EricCodeBJ/Vocalance/ee7a35127a6ab9ffe8275389d0fd9d815c6c0b82/icon/check.png"
+            src="https://www.kidevs.xyz/vocalance/lib/assets/icon/check.png"
           />
           <div class="v-w-box v-w-box1"></div>
           <div class="v-w-box v-w-box2"></div>
@@ -78,6 +78,7 @@ class VocalanceJS {
         }
     }
 
+
     // fetch config
     static #FETCH_OPTIONS = {
         method: 'get',
@@ -87,8 +88,11 @@ class VocalanceJS {
     }
 
     #step = -1;
-    #DetectResponse = '';
-    #shouldContinuDetection = false;
+
+    #NotificationSound = {
+        detectionStart: new Audio('https://www.kidevs.xyz/vocalance/lib/assets/sound/record-on.mp3'),
+        detectionEnd: new Audio('https://www.kidevs.xyz/vocalance/lib/assets/sound/record-off.mp3'),
+    }
 
     // Content configuration informations:
     #config = {
@@ -96,10 +100,12 @@ class VocalanceJS {
         app: {
             uuid: '',
             url: {
-                getData:'http://www.kidevs.xyz/vocalance/public/api/app/projects/',
-                saveUsage: 'http://www.kidevs.xyz/vocalance/public/api/app/history/save/'
+                getData:'https://www.kidevs.xyz/vocalance/public/api/app/projects/',
+                saveUsage: 'https://www.kidevs.xyz/vocalance/public/api/app/history/save/'
             },
-            voice: 0
+            voice: -1,
+            voices: '',
+            voiceIndex: -1
         },
         // here, we store user overwritting method content
         on: {
@@ -121,22 +127,28 @@ class VocalanceJS {
         responses : [] // [key: response, ...]
     }
 
+
     // Initiation
     constructor() {
+        let globalContext = this;
 
-        // Store the user's UUID
+        // Load Voices
+        this.#asyncSpeechDetect();
+
+        // Check for vocalance Widget available 
         let widget = document.getElementsByTagName("vocalance-widget");
         if ( widget.length < 1 ) {
             VocalanceJS.#THROWER('VocalanceJS\'s tag not found. Please read carefully the doc');
             return;
         }
+
+        // Check & Store the user's UUID
         let uuid = widget[0].getAttribute("av-key");
         if (uuid == null || (uuid+'').trim() == '') {
             VocalanceJS.#THROWER('AV-Key must not be null');
             return;
         }
         this.#config.app.uuid = uuid;
-
        
         // fetch Question
         let fetchOption = VocalanceJS.#FETCH_OPTIONS;
@@ -145,9 +157,10 @@ class VocalanceJS {
         this.urlCaller(fetchOption);
     }
 
-
     urlCaller(config) {
-        fetch(config.url)
+        fetch(config.url, {
+            'Content-type': 'application/json; charset=utf-8'
+        })
         .then((response) => response.json())
         .then((data) => {
             if (config.callback != null) {
@@ -162,7 +175,6 @@ class VocalanceJS {
         });
     }
 
-
     /**
      * Here, we try to store projet information like:
      * - question information (key, question, response's time)
@@ -176,9 +188,6 @@ class VocalanceJS {
 
             // store project
             globalContext.#collected.project = project;
-
-            // detect & store voice adapted to language
-            globalContext.#config.app.voice = globalContext.#detectVoice(project.language, project.gender);
 
             // Check if project have question
             if ( (project.qestions).length < 1) {
@@ -197,6 +206,9 @@ class VocalanceJS {
                     responseTime: question.response_time
                 });
             });
+
+            // Detect voice
+            globalContext.#detectVoice(project.language, project.gender);
             
             // Run user on ready function
             if ( typeof globalContext.#config.on.ready === 'function' ) {
@@ -211,36 +223,43 @@ class VocalanceJS {
         return index < (this.#collected.questions).length ? this.#collected.questions[index].question : VocalanceJS.#THROWER(`Question in position ${index} not exist`);
     }
 
-
     getAllQuestions() {
         return this.#collected.questions;
     }
-
 
     getResponse(index = 0) {
         return index < (this.#collected.responses).length ? this.#collected.responses[index] : VocalanceJS.#THROWER(`Response in position ${index} not exist`);
     }
 
-
     getAllResponses() {
         return this.#collected.responses;
     }
-
 
     getProject() {
         return this.#collected.project;
     }
 
+    getStep() {
+        return this.#step;
+    }
+
+    setStep(step) {
+       this.#step = step;
+    }
 
     #textToSpeech(message, callback = null) {
+        let globalContext = this;
         this.#chatCreate("boot"+this.#step, VocalanceJS.#ACTOR_BOOT, "...");
         this.#updateSoundBarUI(VocalanceJS.#STATUT_PAUSE);
 
         const utterance = new SpeechSynthesisUtterance(message);
-        // setting voice
-        utterance.voice = this.#config.app.voice;
+        
+        // Detect voice 
+        if (this.#config.app.voice == -1) {
+            this.#config.app.voice = this.#config.app.voices[this.#config.app.voiceIndex];
+        }
 
-        let globalContext = this;
+        utterance.voice =  this.#config.app.voice;
         // on start speaking
         utterance.addEventListener('start', () => {
             this.#updateSoundBarUI(VocalanceJS.#STATUT_BOT_SPEAK);
@@ -248,13 +267,14 @@ class VocalanceJS {
         });
         // onfinish speaking
         utterance.addEventListener('end', () => {
+            this.#updateSoundBarUI(VocalanceJS.#STATUT_PAUSE);
+
             let getCallback = callback;
             if (typeof getCallback === 'function') {
                 getCallback();
             } else {
                 this.#speechToText();
             }
-            this.#updateSoundBarUI(VocalanceJS.#STATUT_PAUSE);
         });
 
         // speak now
@@ -262,45 +282,58 @@ class VocalanceJS {
         
     }
 
-    #speechToText(duration = 10) {
-        this.#shouldContinuDetection = true;
-        this.#DetectResponse = '';
-        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
-        let recognition = new SpeechRecognition();
+    #speechToText() {
         let globalContext = this;
 
+        // Start detection sound
+        (this.#NotificationSound.detectionStart).play();
+
+        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+        let recognition = new SpeechRecognition();
+
         recognition.onstart = () => {
-            if () {
-                playSound
-            }
+            
         };
-        recognition.onspeechend = () => {
-            if (globalContext.#shouldContinuDetection) {
-                recognition.start();
-            } else {
-                globalContext.#collected.responses[globalContext.#collected.questionKeys[globalContext.#step]] = globalContext.#DetectResponse; 
-                globalContext.#DetectResponse = '';
-                recognition.stop();
-                playSound
-                console.log(globalContext.#collected.responses);
-            }
-        };
+        
         recognition.onresult = (result) => {
-            globalContext.#DetectResponse += result.results[0][0].transcript;
+
+            // Start detection sound
+            recognition.stop();
+            globalContext.#NotificationSound.detectionEnd.play();
+
+            let responseKey = globalContext.#collected.questionKeys[globalContext.#step];
+            let userWords = result.results[0][0].transcript;
+
+            this.#chatCreate("user"+this.#step, VocalanceJS.#ACTOR_USER, userWords);
+            globalContext.#collected.responses[responseKey] = userWords; 
+
+            // continue process
+            setTimeout(() => {
+                globalContext.#step++;
+                let responseCallback = globalContext.#config.on.response;
+                if ( (typeof responseCallback === 'function' && responseCallback(globalContext.#step - 1, userWords)) 
+                    || responseCallback == null) {
+                    if ((globalContext.#collected.questions).length > globalContext.#step) {
+                        this.#updateProgressBarUI(parseInt((globalContext.#step * 100) / (globalContext.#collected.questions).length));
+                        globalContext.#textToSpeech(globalContext.getQuestion(globalContext.#step));
+                    } else {
+                        globalContext.finishProcess();
+                    }
+                }
+                
+            }, 300);
         };
 
-        recognition.start();
+        recognition.onerror = (event) => {
+            console.log(event.error);
+            globalContext.speechToText();
+        };
 
-        setTimeout(() => {
-            globalContext.#shouldContinuDetection = false;
-        }, duration);
-
+        recognition.start();        
     }
 
     #detectVoice(lang, gender) {
-        const availableVoices = speechSynthesis.getVoices();
-
+        let globalContext = this;
         let voiceIndex = 0;
         
         if (lang == 'fr') {
@@ -313,13 +346,24 @@ class VocalanceJS {
             if (gender == 'm') {
                 voiceIndex = 6;
             } else {
-                voiceIndex = 3;
+                voiceIndex = 5;
             }
         }
-        
-        return availableVoices[voiceIndex];
+
+        globalContext.#config.app.voiceIndex = voiceIndex;
     }
 
+    #asyncSpeechDetect() {
+        setTimeout(() => {
+            let availableVoices = speechSynthesis.getVoices();
+            if ( (availableVoices).length > 0 ) {
+                this.#config.app.voices = availableVoices;
+            } else {
+                this.#asyncSpeechDetect();
+            }
+        }, 300);
+        
+    }
 
     onReady(closure) {
         if (typeof closure !== 'function') {
@@ -327,21 +371,29 @@ class VocalanceJS {
             return;
         }
         this.#config.on.ready = closure;
-
-        return this;
     }
 
+    onFinish(closure) {
+        if (typeof closure !== 'function') {
+            VocalanceJS.#THROWER('Only a closure function is allowed here. Please refer to VocalanceJS doc');
+            return;
+        }
+        this.#config.on.finish = closure;
+    }
+
+    // Store on finish closure
     start() {
         // Save usage
         let fetchOption = VocalanceJS.#FETCH_OPTIONS;
         fetchOption.url = this.#config.app.url.saveUsage+this.#config.app.uuid;
         fetchOption.callback = null;
-        // this.urlCaller(fetchOption);
+        this.urlCaller(fetchOption);
 
        // showing this
         let newWidget = document.getElementsByTagName("vocalance-widget")[0];
         newWidget.innerHTML = '';
         newWidget.innerHTML = VocalanceJS.#HTMLVocalanceJSWidget;
+        newWidget.classList.remove('vocalance-widget-show', 'vocalance-widget-hide');
         newWidget.classList.add('vocalance-widget-show');
 
         // Start Talking
@@ -349,13 +401,14 @@ class VocalanceJS {
 
     }
 
-
     #startProcess() {
         this.#updateSoundBarUI(VocalanceJS.#STATUT_PAUSE);
         this.#updateProgressBarUI(0);
 
         // Check for intro message
-        if ( this.#collected.project.intro_message != null ) {
+        if ( this.#collected.project.intro_message != null 
+            && this.#collected.project.intro_message != 'null'
+            && this.#collected.project.intro_message != '' ) {
             this.#textToSpeech(this.#collected.project.intro_message, () => {
                 this.#step = 0;
                 this.#textToSpeech(this.#collected.questions[this.#step]["question"]);
@@ -372,38 +425,83 @@ class VocalanceJS {
         if (currentWidget.length > 0) {
             currentWidget[0].classList.replace('vocalance-widget-show', 'vocalance-widget-hide');
             setTimeout(() => {
-                currentWidget.remove();
-            }, 500);
+                currentWidget[0].innerHTML = '';
+            }, 1000);
         }
     }
 
-    goTo(step = 0) {}
+    repeat() {
+        this.#step--;
+    }
 
-    onResponse(){}
+    goTo(step = 0) {
+        this.#step = step;
+    }
 
-    onFinish(){};
+    onResponse (closure) {
+        if (typeof closure !== 'function') {
+            VocalanceJS.#THROWER('Only a closure function is allowed here. Please refer to VocalanceJS doc');
+            return;
+        }
+        this.#config.on.response = closure;
+    }
+
+
+    finishProcess(){
+        let globalContext = this;
+        this.#updateSoundBarUI(VocalanceJS.#STATUT_FINISH);
+        this.#updateProgressBarUI(100);
+
+        // Check for outro message
+        if ( this.#collected.project.outro_message != null 
+            && this.#collected.project.outro_message != 'null'
+            && this.#collected.project.outro_message != '') {
+            this.#textToSpeech(this.#collected.project.outro_message, () => {
+                globalContext.#callRealFinishMethod();
+            });
+        } else {
+            globalContext.#callRealFinishMethod();
+        }
+    };
+
+    #callRealFinishMethod() {
+        this.#updateSoundBarUI(VocalanceJS.#STATUT_FINISH);
+        let globalContext = this;
+        // if user set finish méthod
+        if (globalContext.#config.on.finish != null) {
+            let callback = globalContext.#config.on.finish;
+            callback(globalContext.#collected.responses);
+        }
+
+        setTimeout(() => {
+            globalContext.stop();
+        }, 5000);
+    }
 
 
     #updateProgressBarUI(progress) {
-        document.querySelector("vocalance-widget .v-w-bloc-progession-bar").style.width = progress;
+        document.querySelector("vocalance-widget .v-w-bloc-progession-bar").style.width = progress+"%";
     }
 
     #updateSoundBarUI(statut = VocalanceJS.#STATUT_PAUSE) {
         // remove All statut class
         let soundBar = document.querySelector("vocalance-widget .v-w-soundbar");
-        soundBar.classList.remove(VocalanceJS.#STATUT_BOT_SPEAK);
-        soundBar.classList.remove(VocalanceJS.#STATUT_USER_SPEAK);
-        soundBar.classList.remove(VocalanceJS.#STATUT_FINISH);
-        soundBar.classList.remove(VocalanceJS.#STATUT_ERROR);
+        soundBar.classList.remove(VocalanceJS.#STATUT_BOT_SPEAK, VocalanceJS.#STATUT_USER_SPEAK, VocalanceJS.#STATUT_FINISH, VocalanceJS.#STATUT_ERROR);
 
         // add target statut class
         if (statut != VocalanceJS.#STATUT_PAUSE) {
             soundBar.classList.add(statut);
+
+            if ( statut == VocalanceJS.#STATUT_FINISH ) {
+                let successImg = document.querySelector("vocalance-widget img.vw-chat-badge-success");
+                successImg.classList.remove("vocalance-widget-hide", "vocalance-widget-show");
+                successImg.classList.add("vocalance-widget-show");
+            }
         }
     }
 
+    
     // Chat Method
-
     #chatCreate(reference, actor, message) {
         let newChatMessage;
         let chatBloc = document.querySelector("vocalance-widget .v-w-chat-bloc");
@@ -412,7 +510,7 @@ class VocalanceJS {
             newChatMessage = `<div class="v-w-question-bloc" data-reference='${reference}'>
                                 <p>${message}</p>
                             </div>`;
-        } else if ( actor == VocalanceJS.#ACTOR_BOOT ) {
+        } else if ( actor == VocalanceJS.#ACTOR_USER ) {
             newChatMessage = `<div class="v-w-reponse-bloc" data-reference='${reference}'>
                                 <p>${message}</p>
                             </div>`;
